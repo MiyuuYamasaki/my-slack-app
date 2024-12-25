fc4a40404db08b071e6ff6ec1a91f53eed4aef20
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2024-12-25 16:05:12 +0900

    conect supabase

--- prisma/migrations/20241223053540_update_user_id_to_string/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "StatusRecord" DROP CONSTRAINT "StatusRecord_user_id_fkey";
-
--- AlterTable
-ALTER TABLE "StatusRecord" ALTER COLUMN "user_id" SET DATA TYPE TEXT;
--- prisma/migrations/20241223073338_update_user_id_type/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "StatusRecord" ALTER COLUMN "user_id" DROP NOT NULL,
-ALTER COLUMN "selected_status" DROP NOT NULL;
