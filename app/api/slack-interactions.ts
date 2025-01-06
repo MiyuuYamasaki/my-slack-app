fbb9bee6bd8d23f50158911e4776eb2fbe0bcde7
MiyuuYamasaki <y-miyu-2429@occ.co.jp>
2025-01-06 12:01:20 +0900

    change find to findMany

--- app/api/slack-interactions.ts
+++ app/api/slack-interactions.ts
@@ -438,7 +438,7 @@ const createModal = async (members: string[], channel: string, prisma: any) => {
     // console.log('status:' + existingRecord?.[member]);
     // const status = existingRecord?.[member] || '休暇'; // ステータスが無い場合は "休暇"
 
-    const existingRecord = prisma.record.find({
+    const existingRecord = prisma.record.findMany({
       where: {
         ymd: getFormattedDate(),
         channel_id: channel,
